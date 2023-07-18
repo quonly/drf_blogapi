e9d08fd92364fc731b643eb60a6c7dc71f28b947
quonly <amornthep.mos07@gmail.com>
2023-07-18 17:50:23 +0700

    authen user

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,9 @@
 from rest_framework import generics, permissions
+from django.contrib.auth import get_user_model
 
 from .models import Post
 
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer
 
 
 class PostList(generics.ListCreateAPIView):
@@ -14,3 +15,7 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     # permission_classes = (permissions.IsAdminUser,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+class UserList(generics.ListCreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
